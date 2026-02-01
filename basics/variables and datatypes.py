69f94d4af70ebecd76637b6c6128a568825a8bc4
Sumit Negi <negi759123@gmail.com>
2026-02-01 10:31:02 +0530

    variable file

---
+++ basics/variables and datatypes.py
@@ -0,0 +1,11 @@
+#Variable are the storage jaha ham value store krte h 
+#value kese type ki h vo batate h datatype
+#rules bhi hote h vo yaad rakhna 
+a=10
+b=20.1
+name= "Sumit negi"
+c=True
+print(type(a))
+print(type(b))
+print(type(name))
+print(type(c))
