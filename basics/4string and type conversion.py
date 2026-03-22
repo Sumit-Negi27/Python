39a0818f3dfc1953ea38854c5af493f64d78b0bb
Sumit Negi <negi759123@gmail.com>
2026-03-23 00:39:40 +0530

    update

---
+++ basics/4string and type conversion.py
@@ -0,0 +1,6 @@
+#String thoda extra space leti h 
+#kyuki haar string ka ek unicode hota h and vo usko save krti h 
+#like
+a="A"
+print(ord(a))#unicode find krtaa h character ka 
+print(chr(65))# unicode ko character mai covert krta h 
