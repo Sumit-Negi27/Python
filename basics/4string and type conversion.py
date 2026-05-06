ed7f5689de0a5870d5754890b7edc983df90fd3e
Sumit Negi <negi759123@gmail.com>
2026-05-06 20:12:43 +0530

    added new file

--- basics/4string and type conversion.py
+++ basics/4string and type conversion.py
@@ -4,3 +4,17 @@
 a="A"
 print(ord(a))#unicode find krtaa h character ka 
 print(chr(65))# unicode ko character mai covert krta h 
+
+#indexing = 0 to n left to right and -1 to -n right to left
+name="Sumit"
+print(name[0])
+print(name)
+print(name[-1])
+
+#STRING SLICING 
+name="sumit negi"
+
+#name[start:stop+1:steps]
+
+print(name[0:5:1])
+print(name[6::])
