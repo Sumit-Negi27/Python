d02415adb5c7ec22e500ec2571c9063a9d7b42af
Sumit Negi <negi759123@gmail.com>
2026-05-10 00:38:27 +0530

    edit new file

--- 6operators.py
+++ 6operators.py
@@ -1,5 +1,5 @@
 #operators are symbols that perform operations in variables and values
-#ARITHAMETIC OPERATIONS
+#ARITHAMETIC OPERATIONS----------------------------------------
 # print(10+2)
 # print(1*2)
 # print(10/2)#divide
@@ -7,10 +7,10 @@
 # print(10**2)#power
 # print(10//2)#ismai decimal ki value he gayab ho jaati h
 
-#Assignment Operators
+#Assignment Operators---------------------------------------------------------
 #compound assignment operations
 
-a=20
+# a=20
 #a=a+20 == a+=20
 # '''a-= 
 # a*=
@@ -23,4 +23,21 @@ a=20
 # a+=20
 # print(a)
 
-#COMPARISION OPERATION
+#COMPARISION OPERATION (hamesa true yah false result aayegaa)________________
+
+#== ; < ; > ; <= ; >= ; != 
+#strings mai bhi use ho sktaa h yaha pr ascii values compare hoti h
+
+# print(ord("A"))
+# print(ord("B"))
+
+# print("A">"B")
+
+#Logical operator and, or, not
+
+# print(12>10 and 10==10)
+# print(10==0 or 20>0)
+# l=[1,2,3]
+# print(1 not in l )
+# print(not 10==10)
+
---
+++ 7conditional statement.py
@@ -0,0 +1,22 @@
+#CONDITIONAL STATEMENT
+# if-else ; if-elif-else ;
+
+# if 3>4:
+#     print("shi h")
+# else:
+#     print("glt h bhai ")
+
+a=input("enter the alphabet : ")
+
+if a=="A" or a=="a":
+    print("it is vowel")
+elif a=="E" or a=="e":
+    print("it is vowel")
+elif a=="i" or a=="I":
+    print("it is vowel")
+elif a=="o" or a=="O":
+    print("it is vowel")
+elif a=="u" or a=="U":
+    print("it is vowel")
+else:
+    print("not vowel")
