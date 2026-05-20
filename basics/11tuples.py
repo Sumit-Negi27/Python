acb93706cfa0b5b4692b9cedd58401d217a2f944
Sumit Negi <negi759123@gmail.com>
2026-05-20 17:38:30 +0530

    update

---
+++ basics/11tuples.py
@@ -0,0 +1,11 @@
+#Tuples 
+#terminology - immutable , duplicate valus allow h 
+#tuples ko bhi index ke through access kr painge
+#hetrogenous hote h means different data type rakh skte h
+a=(1,2,3,4,5,5,5,6,6.6,"sum")
+print(a.index(5) )#help to find the index of tht element
+print(a.count(5))#kitni bar repeat hua h 
+#unpacking the tuple
+e,b,c,d=(2,3,4,5)
+print(e,b,c,d)
+#smallest tuple (1,)
---
+++ basics/12.set.py
@@ -0,0 +1,10 @@
+#SETS
+a={1,2,3,4,5}
+#mutable ; hetrogenous(but not everything) ; unordered hote h ; no duplicate allowed
+# ISMAI HASH FN USE HOTE H VALUES STORE KRNE KE LIYEEE
+#LIST AND DICTIONARY ALLOW NI HOGAA  baaki shi h 
+#Methods
+a.add(10)
+a.remove(1)#RAISE ERROR IF NOT FOUND
+a.discard #NO ERROR RISEIF NOT FOUND
+a.clear()#remove all element
